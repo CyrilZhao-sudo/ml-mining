0fe9161c686d61b7b3948f7c4b484be353f962a8
zhaochen <409192485@qq.com>
2020-11-02 23:57:18 +0800

    a

---
+++ oop/feature_oop/继承.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Group: MI
+# Author: zhao chen
+# Date: 2020-11-01
+
--- xmml/tools/feature_discretize.py
+++ xmml/tools/feature_discretize.py
@@ -5,7 +5,7 @@
 import pandas as pd
 import numpy as np
 from joblib import Parallel, delayed
-
+from math import *
 
 class Discretize:
     def __init__(self, df, cons_features, cate_features, label_identify="label", spec_values=None):
@@ -31,11 +31,29 @@ def get_df_summary(df, label_name, feat_name, is_factor=False):
     pass
 
 
-from scipy.stats import chisquare,  chi2_contingency, chi2, fisher_exact
-def chi_square_pval(b, o):
-    a = b.get("N") * b.get("E")
-    b = b.get("N") - a
-    c = o.get("N") * o.get("E")
-    d = o.get("N") - c
-    pass
+from scipy.stats import chisquare,  chi2_contingency, chi2, fisher_exact, norm
+
+def chi_square_pval(p, q):
+    a = p.get("N") * p.get("E")
+    b = p.get("N") - a
+    c = q.get("N") * q.get("E")
+    d = q.get("N") - c
+    m = np.array([[a, b], [c, d]])
+    if np.any(m < 100):
+        _, p = fisher_exact(m)
+    else:
+        I, E1, dsq1 = p.get("N"), p.get("E"), p.get("V")
+        J, E2, dsq2 = q.get("N"), q.get("E"), q.get("V")
+        K = I + J
+        Et = (E1*I + E2*J) / K
+        if dsq1 is None:
+            dsq1 = 0
+        if dsq2 is None:
+            dsq2 = 0
+        dsqt = (dsq1*(I-1) + I*E1*E1 + dsq2*(J-1) + J*E2*E2 - K*Et*Et) / (K-1)
+        p = 2 * norm().pdf(-sqrt(I*J/K/dsqt) * abs(E1-E2))
+
+    return p
+
+
 
